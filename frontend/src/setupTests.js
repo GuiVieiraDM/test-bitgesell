f960b9880a0f18404762e420c0487190b9d37fbf
Guilherme Vieira <guivieiradm@gmail.com>
2025-07-02 16:02:40 -0300

    Update setupTests.js
    Removing some comments

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,10 +1,5 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-// Mock do console.error para evitar logs desnecessários nos testes
 const originalError = console.error;
 beforeAll(() => {
   console.error = (...args) => {
@@ -22,7 +17,6 @@ afterAll(() => {
   console.error = originalError;
 });
 
-// Mock do IntersectionObserver se necessário
 global.IntersectionObserver = class IntersectionObserver {
   constructor() {}
   disconnect() {}
@@ -30,7 +24,6 @@ global.IntersectionObserver = class IntersectionObserver {
   unobserve() {}
 };
 
-// Mock do ResizeObserver se necessário
 global.ResizeObserver = class ResizeObserver {
   constructor() {}
   disconnect() {}
